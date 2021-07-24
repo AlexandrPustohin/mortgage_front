a7cd7775166e3bc9fd8fce4f3c241cdefc688436
AlexandrPustohin <kich_alex654@mail.ru>
2021-07-24 12:55:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,87 @@
+var app = angular.module("MortgageAppl", []);
+ 
+// Controller Part
+app.controller("MortgageController", function($scope, $http) {
+ 
+ 
+    $scope.mortgages = [];
+    $scope.mortgageForm = {
+        customer: "Пустохин А Е",
+		passport: "19 02 730670",
+        address: "Карла-Маркса 113 б",
+        phon: "89211400904",
+        summa: "5000000",
+        duration: "10",
+        subject: "Квартира на Ленинградской 150",
+        supplier: "Агенство Этажи",
+        supAddress: "Победы 15",
+        inn: "353003514634"
+		
+		
+    };
+ 
+   
+    // HTTP POST  
+    // Call: http://localhost:8080/mortgage
+    $scope.submitMortgage = function() {
+ 
+        var method = "";
+        var url = "";
+        method = "POST";
+        url = ' http://localhost:8088/mortgage';
+        
+ 
+        $http({
+            method: method,
+            url: url,
+            data: angular.toJson($scope.mortgageForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(_success, _error);
+    };
+ 
+    $scope.createMortgage = function() {
+        _clearFormData();
+    }
+ 
+    
+ 
+    function _success(res) {
+        //_refreshEmployeeData();
+		var data = res.data;
+        var status = res.status;
+        var header = res.header;
+        var config = res.config;
+		if ( typeof data.message== "undefined" ) 
+			alert("Заявка сохранена.");
+		else{
+			alert(data.message);
+		}
+        //_clearFormData();
+		
+    }
+ 
+    function _error(res) {
+        var data = res.data;
+        var status = res.status;
+        var header = res.header;
+        var config = res.config;
+        alert("Error: " + status + ":" + data.message);
+    }
+ 
+    // Clear the form
+    function _clearFormData() {
+        $scope.mortgageForm.empName = "";
+		customer= "";
+		passport= "";
+        address = "";
+        phon= "";
+        summa= "";
+        duration= ""; 
+        subject= "";
+        supplier= "";
+        supAddress= "";
+        inn= ""
+    };
+});
